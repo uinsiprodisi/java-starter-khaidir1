ab830c11a5edcecf547b9194b413c22632339d68
Khaidir Ali <haidir1140@gmail.com>
2026-02-23 17:15:23 +0800

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) { 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nama: ");
+        String nama = input.nextLine();
+        
+        System.out.print("Masukkan nim: ");
+        // NIM sebaiknya menggunakan String, bukan int
+        String nim = input.nextLine(); 
+        
+        System.out.print("Masukkan prodi: ");
+        String prodi = input.nextLine();
+        
+        System.out.print("Alasan Belajar Java: ");
+        String alasan = input.nextLine();
+        
+        System.out.println("\n--- Hasil Output ---");
+        System.out.println("Halo " + nama + ", dengan NIM " + nim + " dari program studi " + prodi + ".");
+        System.out.println("Alasan kamu belajar: " + alasan);
+        System.out.println("Selamat belajar JAVA!");
+
+        input.close(); 
     }
 }
